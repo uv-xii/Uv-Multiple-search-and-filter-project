b90bead964245cc6c299d3c95f6ed4e9f1331dbf
YUVRAJ SINGH <yuvi.04.01.2007@gmail.com>
2026-03-28 07:48:37 +0530

    Update Vite config to remove Tailwind CSS
    Removed Tailwind CSS plugin and set base path.

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+  plugins: [react()],
+  base: '/Uv-Multiple-search-and-filter-project/' // 👈 IMPORTANT
 })
